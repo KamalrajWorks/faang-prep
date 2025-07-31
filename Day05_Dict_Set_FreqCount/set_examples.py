0237b91b6b5128e13343cfc3906626f5a427a732
KamalrajWorks <kamalrajworks@gmail.com>
2025-07-31 23:35:52 +0530

    Day05: Added set_examples.py - basics of sets

---
+++ Day05_Dict_Set_FreqCount/set_examples.py
@@ -0,0 +1,7 @@
+# Set examples
+nums = {1, 2, 3, 4}
+nums.add(5)
+nums.remove(2)
+print("Set after operations:", nums)
+print("Union:", nums.union({3,6}))
+print("Intersection:", nums.intersection({1,5,6}))
