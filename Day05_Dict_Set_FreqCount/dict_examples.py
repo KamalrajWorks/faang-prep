5f5b4e5750bfb6d6743d8ba8dc77e4bf4216ebc9
KamalrajWorks <kamalrajworks@gmail.com>
2025-07-31 23:33:10 +0530

    Day05: Added dict_examples.py - basics of dictionaries

---
+++ Day05_Dict_Set_FreqCount/dict_examples.py
@@ -0,0 +1,7 @@
+# Dictionary examples
+student = {"name": "Kamal", "age": 21, "dept": "CSE"}
+print("Student name:", student["name"])
+student["age"] = 22
+print("Updated age:", student["age"])
+print("Keys:", student.keys())
+print("Values:", student.values())
