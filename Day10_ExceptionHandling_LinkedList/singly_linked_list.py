99f142e8a171db620f1c50d13111ee174286ddb9
KamalrajWorks <kamalrajworks@gmail.com>
2025-08-06 23:37:19 +0530

    Day10: Implemented Singly Linked List

---
+++ Day10_ExceptionHandling_LinkedList/singly_linked_list.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
+
+# Driver code
+ll = LinkedList()
+ll.insert(10)
+ll.insert(20)
+ll.insert(30)
+print("Linked List elements:")
+ll.display()
